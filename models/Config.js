bda2624923eb5ac006909644f1bc288fe37cd3da
Gabriel Ilochi <gabriel.ilochi@konga.com>
2022-05-23 23:59:07 +0100

    Added config schema and dto validators

---
+++ models/Config.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const joi = require("joi");
+const Schema = mongoose.Schema;
+
+const ConfigDataSchema = new Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    path: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    value: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ['enabled', 'disabled'],
+        default: 'enabled',
+    }
+}, { timestamps: true});
+
+const ConfigData = mongoose.model('ConfigData', ConfigDataSchema);
+
+const validateConfigDataDto = (configDataPayload) => {
+    const configData = joi.object({
+        name: joi.string().required(),
+        path: joi.string().required(),
+        value: joi.string().required()
+    });
+    return configData.validate(configDataPayload);
+}
+module.exports = {
+    ConfigData,
+    validateConfigDataDto
+};
+
+
