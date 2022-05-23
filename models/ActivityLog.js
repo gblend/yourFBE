5fee980a02e39319ad6016637ed7cf9347b0081a
Gabriel Ilochi <gabriel.ilochi@konga.com>
2022-05-23 23:51:38 +0100

    Added activity log model

---
+++ models/ActivityLog.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const joi = require('joi');
+const Schema = mongoose.Schema;
+
+const ActivityLogSchema = new Schema({
+    action: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    resourceName: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+}, { timestamps: true });
+
+const ActivityLog = mongoose.model('ActivityLog', ActivityLogSchema);
+
+const validateActivityLogDto = (activityLogSchema) => {
+    const activityLog = joi.object({
+        action: joi.string().required(),
+        resourceName: joi.string().required(),
+        user: joi.object().required()
+    });
+    return activityLog.validate(activityLogSchema);
+}
+
+module.exports = {
+    ActivityLog,
+    validateActivityLogDto
+}
