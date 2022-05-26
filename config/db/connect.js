1c412e382dcaf01550d273e6a288e9245f5decb2
Gabriel Ilochi <gabriel.ilochi@konga.com>
2022-05-26 23:38:07 +0100

    Added catch and logs

--- config/db/connect.js
+++ config/db/connect.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose');
+const {logger} = require('../../lib/utils');
+const {StatusCodes} = require('http-status-codes');
 
-const connectDB = (url) => {
-    return mongoose.connect(url);
+const connectDB = async (url) => {
+    try {
+        await mongoose.connect(url);
+        logger.info(`Database connection established`);
+    } catch (err) {
+        logger.error(`${StatusCodes.INTERNAL_SERVER_ERROR} - Database connection failed: ${err.message}`);
+    }
 };
 
 module.exports = connectDB;
