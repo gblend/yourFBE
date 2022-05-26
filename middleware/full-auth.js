e9aa34ea6418c4bbf0816f2acc72acd0356be9ee
Gabriel Ilochi <gabriel.ilochi@konga.com>
2022-05-26 22:52:33 +0100

    Added full auth middleware methods

---
+++ middleware/full-auth.js
@@ -0,0 +1,52 @@
+const CustomError = require('../lib/errors');
+const { isTokenValid } = require('../lib/utils/jwt');
+const {adaptRequest, logger, constants} = require("../lib/utils");
+const {StatusCodes} = require("http-status-codes");
+
+const authenticateUser = async (req, res, next) => {
+  const {headers: {authorization}, cookies, method, path} = adaptRequest(req);
+  let token;
+  // check header
+  if (authorization && authorization.startsWith('Bearer')) {
+    token = authorization.split(' ')[1];
+  }
+  // check cookies
+  else if (cookies.token) {
+    token = cookies.token;
+  }
+
+  if (!token) {
+    logger.info(`${StatusCodes.UNAUTHORIZED} - Authentication error - ${method} ${path}`)
+    throw new CustomError.UnauthenticatedError(constants.auth.AUTHENTICATION_INVALID);
+  }
+  try {
+    const payload = isTokenValid(token);
+
+    // Attach the user and his permissions to the req object
+    req.user = {
+      userId: payload.user.userId,
+      role: payload.user.role,
+    };
+
+    next();
+  } catch (error) {
+    logger.info(`${error.statusCode} - Authentication invalid: ${error.message} - ${method} ${path}`)
+    throw new CustomError.UnauthenticatedError(constants.auth.AUTHENTICATION_INVALID);
+  }
+};
+
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    const {method, path, user} = adaptRequest(req);
+
+    if (!roles.includes(user.role)) {
+      logger.info(`${StatusCodes.UNAUTHORIZED} - Unauthorized access error - ${method} ${path}`)
+      throw new CustomError.UnauthorizedError(
+        'Unauthorized to access this route'
+      );
+    }
+    next();
+  };
+};
+
+module.exports = { authenticateUser, authorizeRoles };
