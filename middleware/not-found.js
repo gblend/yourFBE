d1e7b3261f2e37c9e60e599231597f8020090462
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-05-28 17:03:04 +0100

    Updated response

--- middleware/not-found.js
+++ middleware/not-found.js
@@ -1,3 +1,6 @@
-const notFound = (_req, res) => res.status(404).send('Route does not exist')
+'use strict';
+
+const {StatusCodes} = require('http-status-codes');
+const notFound = (_req, res) => res.status(StatusCodes.NOT_FOUND).json({'status': StatusCodes.NOT_FOUND, message: 'Route does not exist.'})
 
 module.exports = notFound
