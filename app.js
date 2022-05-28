5af01f2cb4b82d4e089c521da1b1d929c2d48d66
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-05-28 17:10:51 +0100

    Added http status codes, app status  and express async errors

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 'use strict';
 
-require('express-async-errors')
-require('dotenv').config();
-
 const {
     morgan,
     errorHandlerMiddleware,
@@ -28,33 +25,41 @@ const {
     logRouter,
     config,
     app,
-    express
+    express,
+    appStatus,
+    StatusCodes,
+    expressAsyncErrors,
 } = require('./startup');
 
+expressAsyncErrors();
 app.use(helmet());
 app.use(xss());
 app.use(mongoSanitize());
 app.set('trust proxy', 1);
 app.use(
     rateLimit({
-    windowMs: config.rateLimiter.windowMs,
-    max: config.rateLimiter.max
+        windowMs: config.rateLimiter.windowMs,
+        max: config.rateLimiter.max
     })
 );
 app.use(express.json());
-app.use(express.urlencoded({ extended: false}));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser(config.jwt.secret));
 app.use(decodeCookies);
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload({useTempFiles: true}));
 
-if(config.app.env === 'development') {
+if (config.app.env === 'development') {
     app.use(morgan('dev'));
 }
 
-app.get('/api/v1/', (_req, res) => {
-    return res.json({'status': '200', message: 'YourFeeds backend service is running.'});
+app.get('/api/v1/status', (_req, res) => {
+    return res.json({
+        'status': StatusCodes.OK,
+        message: `${config.app.name} backend service is running.`,
+        info: appStatus.compile()
+    });
 });
 app.get('/api/v1/doc', (_req, res) => {
     return res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -74,7 +79,7 @@ app.use(errorHandlerMiddleware);
 const start = async () => {
     await connectDB(config.database.uri);
     app.listen(config.app.port, () => {
-        logger.info(`server listening on port ${config.app.port}`);
+        logger.info(`${config.app.name} server running: ${config.app.baseUrlDev}:${config.app.port}`);
     });
 }
 start().then(_ => _);
