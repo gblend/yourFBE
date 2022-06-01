59c7809e9c1fcf8feb391a9867c0a41c682e945f
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-02 00:48:38 +0100

    Added test case for validation of invalid token

--- tests/unit/validateJWT.test.js
+++ tests/unit/validateJWT.test.js
@@ -6,5 +6,9 @@ describe('CreateJWT', () => {
 		const token = createJWT({user: 'testUser'});
 		expect(isTokenValid(token)).toBeTruthy();
 	});
-	
+
+	it.only('should throw error when provided token is invalid', () => {
+		const token = 'fake token';
+		expect(() => isTokenValid(token)).toThrow('jwt malformed');
+	});
 });
