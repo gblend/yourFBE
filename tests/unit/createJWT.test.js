2193db0f37be8d500cfefd44c731c3f1c2244cbb
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-02 00:36:30 +0100

    Removed run only option

--- tests/unit/createJWT.test.js
+++ tests/unit/createJWT.test.js
@@ -2,16 +2,16 @@ const {createJWT} = require('../../lib/utils');
 
 
 describe('CreateJWT', () => {
-	it.only('should return created JWT string token when payload is provided', () => {
+	it('should return created JWT string token when payload is provided', () => {
 		expect(typeof (createJWT({}))).toBe('string');
 		expect(createJWT({}).length).toBeGreaterThan(0);
 	});
 
-	it.only('should throw an error when payload is undefined', () => {
+	it('should throw an error when payload is undefined', () => {
 		expect(() => createJWT(undefined)).toThrow('payload is required');
 	});
 
-	it.only('should throw an error when payload is null (not a plain object)', () => {
+	it('should throw an error when payload is null (not a plain object)', () => {
 		expect(() => createJWT(null)).toThrow();
 	});
 
