93f51dfd698dd4ef5e713533bc696e9c4d77a02d
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-01 23:01:18 +0100

    Added test case for error when uploaded file size exceeds maximum allowed size

--- tests/unit/cloudinaryFileUpload.test.js
+++ tests/unit/cloudinaryFileUpload.test.js
@@ -24,4 +24,13 @@ describe('UploadImage', () => {
 		}
 	});
 
+	it('should return error when file to upload exceeds maximum size of 2mb (2097152)', async () => {
+		try {
+			await uploadImage({files: {uploadImage: {size: 3145728, mimetype: 'image/'}}});
+		} catch (err) {
+			expect(err.statusCode).toEqual(400);
+			expect(typeof(err.message)).toBe('string');
+			expect(err.message.length).toBeGreaterThan(0);
+		}
+	});
 });
