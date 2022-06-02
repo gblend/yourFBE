d5c280e5aa9d0843ab65be2e7dbefa54a71e48dd
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-02 19:22:31 +0100

    Added test case for decoding of malformed token

--- tests/unit/decodeJWT.test.js
+++ tests/unit/decodeJWT.test.js
@@ -24,4 +24,13 @@ describe('DecodeJWT', () => {
 		expect(result).toMatchObject({signedCookies: {}, cookies: {}, user: {user: 'testUser', role: 'user'}});
 	});
 
+	it('should null for user when provided token is invalid', async () => {
+		const token = 'fake token';
+		const req = mockRequest({signedCookies: {token}, cookies: {token}});
+		const res = mockResponse();
+		const next = mockNext(req);
+
+		const decoded = await decodeCookies(req, res, next);
+		expect(decoded).toMatchObject({signedCookies: {}, cookies: {}, user: null});
+	});
 });
