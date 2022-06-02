a2e28110dda9f3af5bed4bade47c62fe46e8886b
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-02 19:20:22 +0100

    Removed only option

--- tests/unit/validateJWT.test.js
+++ tests/unit/validateJWT.test.js
@@ -1,13 +1,13 @@
 const {createJWT, isTokenValid} = require('../../lib/utils');
 
 
-describe('CreateJWT', () => {
-	it.only('should return true if token is valid', () => {
+describe('VerifyJWT', () => {
+	it('should return true if token is valid', () => {
 		const token = createJWT({user: 'testUser'});
 		expect(isTokenValid(token)).toBeTruthy();
 	});
 
-	it.only('should throw error when provided token is invalid', () => {
+	it('should throw error when provided token is invalid', () => {
 		const token = 'fake token';
 		expect(() => isTokenValid(token)).toThrow('jwt malformed');
 	});
