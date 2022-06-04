6f745b4ae21ac7327744d3a419511067bd0ee80f
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-04 09:55:31 +0100

    Handled database connection rejection

--- config/db/connect.js
+++ config/db/connect.js
@@ -1,13 +1,19 @@
 const mongoose = require('mongoose');
 const {logger} = require('../../lib/utils');
 const {StatusCodes} = require('http-status-codes');
+const {CustomAPIError} = require('../../lib/errors');
 
 const connectDB = async (url) => {
     try {
-        await mongoose.connect(url);
-        logger.info(`Database connection established`);
+        return await mongoose.connect(url)
+            .then(connection => {
+                logger.info('Database connection established.');
+                return connection;
+            }).catch(err => {
+                throw new CustomAPIError(`Database connection error: ${err.message}`);
+            });
     } catch (err) {
-        logger.error(`${StatusCodes.INTERNAL_SERVER_ERROR} - Database connection failed: ${err.message}`);
+        logger.error(`${StatusCodes.INTERNAL_SERVER_ERROR} - ${err.message}`);
     }
 };
 
