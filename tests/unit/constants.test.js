9b1b2e5069de8a46cc7c51cc6250a7b0b809dcf4
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-05 22:31:47 +0100

    Added test cases for auth constants

---
+++ tests/unit/constants.test.js
@@ -0,0 +1,19 @@
+const {constants} = require('../../lib/utils');
+
+describe('Constants', () => {
+	it('should return INVALID_CREDENTIALS', () => {
+		expect(constants.auth.INVALID_CREDENTIALS()).toContain('Invalid');
+	});
+
+	it('should return ALREADY_IN_USE', () => {
+		expect(constants.auth.ALREADY_IN_USE()).toContain('is already in use.');
+	});
+
+	it('should return AUTHENTICATION_INVALID', () => {
+		expect(constants.auth.AUTHENTICATION_INVALID).toContain('Authentication invalid.');
+	});
+
+	it('should return SUCCESSFUL', () => {
+		expect(constants.auth.SUCCESSFUL()).toContain('successful.');
+	});
+});
