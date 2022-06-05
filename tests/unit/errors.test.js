f750c57c13a849a66e91bb7367b75760a2e8d309
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-05 22:44:36 +0100

    Added test cases for application errors

--- tests/unit/errors.test.js
+++ tests/unit/errors.test.js
@@ -1,4 +1,4 @@
-const {NotFoundError} = require('../../lib/errors');
+const {NotFoundError, UnauthenticatedError, UnauthorizedError, CustomAPIError, BadRequestError} = require('../../lib/errors');
 const {StatusCodes} = require('../../lib/utils');
 
 describe('NotFoundError', () => {
@@ -7,5 +7,40 @@ describe('NotFoundError', () => {
 		const notFound = new NotFoundError(message);
 		expect(notFound.statusCode).toBe(StatusCodes.NOT_FOUND);
 		expect(notFound.message).toBe(message);
+	});
+});
+
+describe('UnauthenticatedError', () => {
+	it('should return unauthenticated error', () => {
+		const message = 'Authentication failed.';
+		const unauthenticated = new UnauthenticatedError(message);
+		expect(unauthenticated.statusCode).toBe(StatusCodes.UNAUTHORIZED);
+		expect(unauthenticated.message).toBe(message);
+	});
+});
+
+describe('UnauthorizedError', () => {
+	it('should return unauthorized error', () => {
+		const message = 'Unauthorized access.';
+		const unauthorized = new UnauthorizedError(message);
+		expect(unauthorized.statusCode).toBe(StatusCodes.FORBIDDEN);
+		expect(unauthorized.message).toBe(message);
+	});
+});
+
+describe('CustomAPIError', () => {
+	it('should return custom API error', () => {
+		const message = 'Custom message.';
+		const customAPIError = new CustomAPIError(message);
+		expect(customAPIError.message).toBe(message);
+	});
+});
+
+describe('BadRequestError', () => {
+	it('should return bad request error', () => {
+		const message = 'Bad request.';
+		const badRequestError = new BadRequestError(message);
+		expect(badRequestError.statusCode).toBe(StatusCodes.BAD_REQUEST);
+		expect(badRequestError.message).toBe(message);
 	});
 });
