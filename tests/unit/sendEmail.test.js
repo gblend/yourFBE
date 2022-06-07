66528a301bb0eccfe7df146a57d766e3b3ba2712
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-07 08:33:56 +0100

    Fixed test

--- tests/unit/sendEmail.test.js
+++ tests/unit/sendEmail.test.js
@@ -2,28 +2,36 @@ const emailHelpers = require('../../app/lib/utils/email/sendEmail');
 const {generateToken} = require('../../app/lib/utils');
 
 describe('SendEmail', () => {
-	it('should send verification email to the user', () => {
+
+	it('should send verification email to the user', async () => {
 		const verificationEmail = jest.spyOn(emailHelpers, 'sendVerificationEmail');
-		const mockVerificationEmail = ({name, email, verificationToken: token}) => jest.fn().
-		mockReturnValue(emailHelpers.sendVerificationEmail({name, email, verificationToken: token}));
+		const mockVerificationEmail = async ({name, email, verificationToken: token}) =>
+			jest.fn().mockReturnValue(
+				await emailHelpers.sendVerificationEmail({name, email, verificationToken: token})
+			);
+
 		const name = 'Test User';
 		const email = 'test@example.com';
 		const token = generateToken();
-		mockVerificationEmail({name, email, verificationToken: token});
+		emailHelpers.sendVerificationEmail.mockImplementation(() => 'Verification email sent successfully.');
+		await mockVerificationEmail({name, email, verificationToken: token}).catch(_ => _);
 
 		expect(verificationEmail).toHaveBeenCalledTimes(1);
 		expect(verificationEmail).toHaveBeenCalledWith({name, email, verificationToken: token});
 	});
 
-	it('should send reset password email to the user', () => {
+	it('should send reset password email to the user', async () => {
 		const resetPasswordEmail = jest.spyOn(emailHelpers, 'sendResetPasswordEmail');
-		const mockResetPasswordEmail = ({name, email, passwordToken: token}) => jest.fn().
-		mockReturnValue(emailHelpers.sendResetPasswordEmail({name, email, passwordToken: token}));
+		const mockResetPasswordEmail = async ({name, email, passwordToken: token}) =>
+			jest.fn().mockReturnValue(
+				await emailHelpers.sendResetPasswordEmail({name, email, passwordToken: token})
+			);
 
 		const name = 'Reset User';
 		const email = 'reset@example.com';
 		const token = generateToken();
-		mockResetPasswordEmail({name, email, passwordToken: token});
+		emailHelpers.sendResetPasswordEmail.mockImplementation(() => 'Reset password email sent successfully.');
+		await mockResetPasswordEmail({name, email, passwordToken: token}).catch(_ => _);
 
 		expect(resetPasswordEmail).toHaveBeenCalledTimes(1);
 		expect(resetPasswordEmail).toHaveBeenCalledWith({name, email, passwordToken: token});
