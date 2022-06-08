81ab2f1159ccaa46001d15f402278617fafd21b2
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-08 18:53:45 +0100

    Updated error handler

--- app/middleware/error-handler.js
+++ app/middleware/error-handler.js
@@ -7,7 +7,7 @@ const errorHandlerMiddleware = (err, req, res, _next) => {
     let customError = {
         // set default
         statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
-        msg: err.message || 'Something went wrong try again later.',
+        message: err.message || 'Something went wrong try again later.',
     };
 
     if (err.name === 'ValidationError') {
@@ -18,19 +18,19 @@ const errorHandlerMiddleware = (err, req, res, _next) => {
     }
 
     if (err.code && err.code === 11000) {
-        customError.msg = `Duplicate value entered for ${Object.keys(
+        customError.message = `Duplicate value entered for ${Object.keys(
             err.keyValue
         )} field, please choose another value.`;
         customError.statusCode = 400;
     }
 
     if (err.name === 'CastError') {
-        customError.msg = `No item found with id : ${err.value}`;
+        customError.message = `No resource found with id: ${err.value}`;
         customError.statusCode = 404;
     }
 
-    logger.error(`${customError.statusCode} - ${customError.msg} - ${method} ${path}`);
-    return res.status(customError.statusCode).json({msg: customError.msg});
+    logger.error(`${customError.statusCode} - ${customError.message} - ${method} ${path}`);
+    return res.status(customError.statusCode).json({status: customError.statusCode, message: customError.message});
 };
 
 module.exports = errorHandlerMiddleware;
