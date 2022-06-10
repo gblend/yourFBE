dc7db785dbdcccc934a059c2b58bf634bbccf50d
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-10 10:58:08 +0100

    Updated pagination to include total, pages, current, previous & next

--- app/lib/utils/pagination.js
+++ app/lib/utils/pagination.js
@@ -3,19 +3,18 @@
 const {logger} = require('./logger');
 
 const paginate = (resultData, params = {}) => {
-    const pagination = {}
-
-    if (!params || params === {}) {
-        pagination.pageSize = 10;
-        pagination.pageNumber = 1;
-    } else {
-        if (!params.pageSize) params.pageSize = 10;
-        if (!params.pageNumber) params.pageNumber = 1;
-        pagination.pageSize = params.pageSize < 1 ? 1 : params.pageSize;
-        pagination.pageNumber = params.pageNumber < 1 ? 1 : params.pageNumber;
+    const {pageNumber, pageSize} = params;
+    const pagination = {
+        pageSize: (!pageSize || pageSize < 1) ? 10 : Number(pageSize),
+        pageNumber: (!pageNumber || pageNumber < 1) ? 1 : Number(pageNumber),
     }
-
     pagination.offset = Number((pagination.pageNumber - 1) * pagination.pageSize);
+    pagination.total = resultData.length || 0;
+    pagination.pages = Math.ceil(pagination.total/pagination.pageSize);
+    pagination.current =  pagination.pageNumber;
+    pagination.previous = (pagination.current > 1) ? pagination.current - 1 : pagination.current;
+    pagination.next = (pagination.pages > pagination.current) ? pagination.current + 1 : pagination.current;
+
     const result = resultData.slice(pagination.offset, pagination.pageSize);
     logger.info(JSON.stringify(`${pagination} ${resultData}`));
     return {pagination, result}
