6f7ffebde5e798efd39438185dd3cfd14430cde9
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-12 23:16:27 +0100

    Changed route path to kebab-case

--- app/routes/userRoutes.js
+++ app/routes/userRoutes.js
@@ -17,10 +17,10 @@ const {
 } = require('../controllers/userController');
 
 router.route('/').get(authenticateUser, authorizePermissions('admin'), getAllUsers);
-router.route('/disabledAccounts').get(authenticateUser, authorizePermissions('admin'), getDisabledAccounts);
+router.route('/disabled-accounts').get(authenticateUser, authorizePermissions('admin'), getDisabledAccounts);
 router.route('/admins').get(authenticateUser, authorizePermissions('admin'), getAllAdmins);
-router.route('/updatePassword').patch(authenticateUser, updatePassword);
-router.route('/:id').delete(authenticateUser, authorizePermissions('admin', 'user'), disableUserAccount).patch(authenticateUser, enableUserAccount);
+router.route('/update-password').patch(authenticateUser, updatePassword);
+router.route('/accounts/moderate/:id').delete(authenticateUser, authorizePermissions('admin', 'user'), disableUserAccount).patch(authenticateUser, enableUserAccount);
 router.route('/me').get(authenticateUser, showCurrentUser);
 router.route('/:id').get(authenticateUser, getSingleUser);
 router.route('/update/:id').patch(authenticateUser, updateUser);
