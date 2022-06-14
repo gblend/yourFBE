aeca8da381ef17cba9482130f6808744ad44aa67
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-14 07:16:48 +0100

    Added paths for retrieving activity and polling logs

--- app/routes/logRoutes.js
+++ app/routes/logRoutes.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const {authenticateUser, authorizePermissions} = require('../middleware/authentication');
 
-const {} = require('../controllers/logController');
+const {getActivityLogs, getActivityLog, getPollingLogs, getPollingLog, searchLogs} = require('../controllers/logController');
+
+router.route('/activity').get(authenticateUser, authorizePermissions('admin'), getActivityLogs);
+router.route('/activity/:id').get(authenticateUser, authorizePermissions('admin'), getActivityLog);
+router.route('/polling').get(authenticateUser, authorizePermissions('admin'), getPollingLogs);
+router.route('/polling/:id').get(authenticateUser, authorizePermissions('admin'), getPollingLog);
+router.route('/search').get(authenticateUser, authorizePermissions('admin'), searchLogs);
 
 module.exports = router;
