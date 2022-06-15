16dbcc55e7e4b0a40f92aaee0b866d59cc29686c
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-15 23:57:19 +0100

    Added save for later routes

--- app/routes/savedForLaterRoutes.js
+++ app/routes/savedForLaterRoutes.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {authenticateUser, authorizePermissions} = require('../middleware/authentication');
-const {} = require('../controllers/savedForLaterController');
+const {authenticateUser} = require('../middleware/authentication');
+const {
+	savePostForLater,
+	deletePostSavedForLater,
+	getPostsSavedForLater,
+	getPostSavedForLater,
+	markPostSavedForLaterAsRead,
+} = require('../controllers/savedForLaterController');
+
+router.route('/posts').post(authenticateUser, savePostForLater).get(authenticateUser, getPostsSavedForLater);
+router.route('/posts/:id').get(authenticateUser, getPostSavedForLater)
+	.delete(authenticateUser, deletePostSavedForLater).patch(authenticateUser, markPostSavedForLaterAsRead);
 
 module.exports = router;
