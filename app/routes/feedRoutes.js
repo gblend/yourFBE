dec54207fd54cf41e6b86f6dfbaebcd184c9bcb7
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-15 23:56:43 +0100

    Added feed routes

--- app/routes/feedRoutes.js
+++ app/routes/feedRoutes.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const {authenticateUser, authorizePermissions} = require('../middleware/authentication');
+const {
+	getFeedsByCategory,
+	createFeed,
+	deleteFeed,
+	disableFeedById,
+	disableFeedsByCategoryId,
+	getFeedById,
+	getFeeds,
+	getFeedsByCategoryId,
+	updateFeed,
+} = require('../controllers/feedController');
 
-const {} = require('../controllers/feedController');
+router.route('/').get(authenticateUser, getFeeds)
+	.post(authenticateUser, authorizePermissions('admin'), createFeed);
+router.route('/:id').get(authenticateUser, getFeedById)
+	.delete(authenticateUser, authorizePermissions('admin'), deleteFeed)
+	.patch(authenticateUser, authorizePermissions('admin'), updateFeed);
+router.route('/category').get(authenticateUser, getFeedsByCategory);
+router.route('/category/:id').get(authenticateUser, getFeedsByCategoryId);
+router.route('/disable/:id').delete(authenticateUser, authorizePermissions('admin'), disableFeedById)
+	.patch(authenticateUser, authorizePermissions('admin'), disableFeedsByCategoryId);
 
 module.exports = router;
