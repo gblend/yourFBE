c369553f69100989819cb0fb7ad95a8ab7296f5b
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-16 19:09:44 +0100

    Added feed category routes

--- app/routes/feedCategoryRoutes.js
+++ app/routes/feedCategoryRoutes.js
@@ -2,6 +2,18 @@ const express = require('express');
 const router = express.Router();
 const {authenticateUser, authorizePermissions} = require('../middleware/authentication');
 
-const {} = require('../controllers/feedCategoryController');
+const {
+	getCategories,
+	createCategory,
+	updateCategory,
+	deleteCategory,
+	disableCategory,
+	getCategoryById,
+} = require('../controllers/feedCategoryController');
+
+router.route('/').get(getCategories).post(authenticateUser, authorizePermissions('admin'), createCategory);
+router.route('/:id').get(getCategoryById).delete(authenticateUser, authorizePermissions('admin'), deleteCategory)
+	.patch(authenticateUser, authorizePermissions('admin'), updateCategory);
+router.route('/disable/:id').delete(authenticateUser, authorizePermissions('admin'), disableCategory);
 
 module.exports = router;
