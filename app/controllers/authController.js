0347abbb0c0b0267834fe01e8f7863af0a6a58c4
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-18 19:19:49 +0100

    Added method for forgot  password

--- app/controllers/authController.js
+++ app/controllers/authController.js
@@ -127,8 +127,33 @@ const logout = async (req, res) => {
 	res.status(StatusCodes.NO_CONTENT).json({});
 }
 
+const forgotPassword = async (req, res) => {
+	const {body: {email}} = adaptRequest(req);
+	if (!email) {
+		throw new BadRequestError('Please enter a valid email');
+	}
+
+	const user = await User.findOne({email});
+	if (user) {
+		const passwordToken = generateToken();
+
+		// queue reset password email
+		queueErrorMsg = 'Unable to queue reset password email, please try again';
+		queueName = config.amqp.resetEmailQueue;
+		await pushToQueue(queueName, queueErrorMsg, {name: user.firstname, email: user.email, passwordToken});
+
+		const tenMinutes = 1000 * 10 * 60;
+		const passwordTokenExpirationDate = new Date(Date.now() + tenMinutes);
+		user.passwordToken = createHash(passwordToken);
+		user.passwordTokenExpirationDate = passwordTokenExpirationDate;
+		await user.save();
+	}
+	res.status(StatusCodes.OK).json({message: 'Please check your email for reset link'});
+}
+
 module.exports = {
 	register,
 	logout,
 	login,
+	forgotPassword,
 }
