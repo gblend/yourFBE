18827fc541416ff6679ca02da432470dd2db4ae4
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-19 19:25:54 +0100

    Added method to fetch all active users

---
+++ app/controllers/userController.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const {User, validateUpdateUser, validateUpdatePassword} = require('../models/User');
+const {StatusCodes} = require('http-status-codes');
+const NotFoundError = require('../lib/errors/not-found');
+const mongoose = require('mongoose');
+const {BadRequestError, UnauthenticatedError, UnauthorizedError} = require('../lib/errors');
+const {
+    checkPermissions,
+    adaptRequest,
+    constants,
+    formatValidationError,
+    logger,
+    redisGetBatchRecords,
+    redisSetBatchRecords, paginate,
+} = require('../lib/utils');
+const {config} = require('../config/config');
+const {saveActivityLog} = require('../lib/dbActivityLog');
+
+const getAllUsers = async (req, res) => {
+    const {method, path: _path, queryParams: {sort, pageSize, pageNumber}} = adaptRequest(req);
+    let users = await redisGetBatchRecords(config.cache.allUsersCacheKey);
+    if (users.length < 1) {
+        users = await User.find({role: 'user', status: 'enabled'}).select(['-password', '-verificationToken']);
+        if (users.length < 1) {
+            logger.info(`${StatusCodes.NOT_FOUND} - No user found for get_all_users - ${method} ${_path}`);
+            throw new NotFoundError('No user found.');
+        }
+        await redisSetBatchRecords(config.cache.allUsersCacheKey, users);
+    }
+
+    if (sort) {
+        const sortFields = sort.split(',').join(' ');
+        users.sort(sortFields);
+    }
+
+    let {pagination, result} = paginate(users, {pageSize, pageNumber});
+    // const totalLogs = await logsQuery.estimatedDocumentCount().exec();
+    logger.info(JSON.stringify(users));
+    return res.status(StatusCodes.OK).json({message: 'Users fetched successfully', data: {users: result, pagination}});
+}
+
+module.exports = {
+    getAllUsers,
+}
