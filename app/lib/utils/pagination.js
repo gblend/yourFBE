8caa072173c79e34bb8cd62d3136ad4e5a388f40
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-22 21:25:54 +0100

    Added check for mongoose collection and schema

--- app/lib/utils/pagination.js
+++ app/lib/utils/pagination.js
@@ -1,22 +1,28 @@
 'use strict';
 
-const {logger} = require('./logger');
-
-const paginate = (resultData, params = {}) => {
+const paginate = async (resultData, params = {}) => {
     const {pageNumber, pageSize} = params;
     const pagination = {
         pageSize: (!pageSize || pageSize < 1) ? 10 : Number(pageSize),
         pageNumber: (!pageNumber || pageNumber < 1) ? 1 : Number(pageNumber),
     }
     pagination.offset = Number((pagination.pageNumber - 1) * pagination.pageSize);
-    pagination.total = resultData.length || 0;
-    pagination.pages = Math.ceil(pagination.total/pagination.pageSize);
+
+    let result;
+    if (Object.keys(resultData).includes('mongooseCollection') || Object.keys(resultData).includes('schema')) {
+        pagination.total = await resultData.clone().countDocuments();
+        pagination.pages = Math.ceil(pagination.total/pagination.pageSize);
+        result = resultData.skip(pagination.offset).limit(pagination.pageSize);
+    } else {
+        pagination.total = resultData.length || 0;
+        pagination.pages = Math.ceil(pagination.total/pagination.pageSize);
+        result = resultData.slice(pagination.offset, pagination.pageSize);
+    }
+
     pagination.current =  pagination.pageNumber;
     pagination.previous = (pagination.current > 1) ? pagination.current - 1 : pagination.current;
     pagination.next = (pagination.pages > pagination.current) ? pagination.current + 1 : pagination.current;
 
-    const result = resultData.slice(pagination.offset, pagination.pageSize);
-    logger.info(JSON.stringify(`${pagination} ${resultData}`));
     return {pagination, result}
 }
 
