0b554f42d5a0ee384ab14a9dcdc4e172ed5f6fe6
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-23 21:32:46 +0100

    Updated message ternary operator

--- app/middleware/resInterceptor.js
+++ app/middleware/resInterceptor.js
@@ -9,10 +9,9 @@ const resInterceptor = (_req, res, next) => {
 	let originJson = res.json;
 	res.json = ({ data = {}, message = '' }) => {
 		let statusCode = res.statusCode;
-		data = (!data || Object.keys(data).length === 0) ? {} : data;
 		const statusText = (![StatusCodes.OK, StatusCodes.NO_CONTENT, StatusCodes.CREATED].includes(statusCode)) ? 'error' : 'success';
-		message = (message.length < 1 && statusText === 'success') ? successMessage : message;
-		message = (message.length < 1 && statusText === 'error') ?  errorMessage : message;
+		message = (!message && statusText === 'success') ? successMessage : message;
+		message = (!message && statusText === 'error') ?  errorMessage : message;
 
 		return originJson.call(res,{status: statusText, message, data});
 	}
