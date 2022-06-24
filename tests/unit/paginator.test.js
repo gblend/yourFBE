593ad71e961a421dc0f3cdfcc0d941d6ff68fad4
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-24 21:59:39 +0100

    Updated paginate method to use promise

--- tests/unit/paginator.test.js
+++ tests/unit/paginator.test.js
@@ -13,35 +13,35 @@ describe('Paginator', () => {
 
     describe('Paginate Array Data', () => {
         describe('paginate without data', () => {
-            it('without pageNumber, pageSize, and empty array', () => {
+            it('without pageNumber, pageSize, and empty array', async () => {
                 expected.pagination.pageSize = 10;
                 expected.pagination.pages = 0;
                 expected.pagination.next = 1;
 
-                expect(paginate([], {})).toEqual(expected);
+                expect(await paginate([], {})).toEqual(expected);
             });
 
-            it('with pageNumber and empty array', () => {
+            it('with pageNumber and empty array', async () => {
                 params.pageNumber = 1;
                 expected.pagination.pageSize = 10;
                 expected.pagination.pages = 0;
                 expected.pagination.next = 1;
 
-                expect(paginate([], params)).toEqual(expected);
+                expect(await paginate([], params)).toEqual(expected);
             });
 
-            it('with pageSize and empty array', () => {
+            it('with pageSize and empty array', async () => {
                 params.pageSize = 1;
                 expected.pagination.pageSize = 1;
                 expected.pagination.pages = 0;
                 expected.pagination.next = 1;
 
-                expect(paginate([], params)).toEqual(expected);
+                expect(await paginate([], params)).toEqual(expected);
             });
         });
 
         describe('paginate with data', () => {
-            it('with pageNumber, pageSize, and data array', () => {
+            it('with pageNumber, pageSize, and data array', async () => {
                 params.pageSize = 5;
                 params.pageNumber = 1;
                 expected.pagination.pageSize = 5;
@@ -50,10 +50,10 @@ describe('Paginator', () => {
                 expected.pagination.next = 2;
                 expected.result = data.slice(0, 5)
 
-                expect(paginate(data, params)).toEqual(expected);
+                expect(await paginate(data, params)).toEqual(expected);
             });
 
-            it('with pageNumber and data array', () => {
+            it('with pageNumber and data array', async () => {
                 params.pageNumber = 1;
                 expected.pagination.pageSize = 10;
                 expected.pagination.total = 6;
@@ -62,10 +62,10 @@ describe('Paginator', () => {
                 expected.result = data.slice(0, 10)
 
 
-                expect(paginate(data, params)).toEqual(expected);
+                expect(await paginate(data, params)).toEqual(expected);
             });
 
-            it('with pageSize and data array', () => {
+            it('with pageSize and data array', async () => {
                 params.pageSize = 5;
                 expected.pagination.pageSize = 5;
                 expected.pagination.total = 6;
@@ -73,7 +73,7 @@ describe('Paginator', () => {
                 expected.pagination.next = 2;
                 expected.result = data.slice(0, 5)
 
-                expect(paginate(data, params)).toEqual(expected);
+                expect(await paginate(data, params)).toEqual(expected);
             });
         });
     });
