648ac0e12f81299203c0cc3c2d3849b5bcb6b5e1
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-30 04:01:03 +0100

    Added test case for initializing an app instance

--- tests/integration/server.test.js
+++ tests/integration/server.test.js
@@ -1,7 +1,15 @@
 const {tearDownTestConnection} = require('../helpers/connection_teardown');
+const {connection} = require('mongoose');
+const app = require('../../server');
 
 describe('App', () => {
 	afterAll( async () => {
 		await tearDownTestConnection();
+	});
+
+	it('should successfully initialize app instance',() => {
+		expect(Object.keys((connection.models)).length).toBeGreaterThan(0);
+		expect(app).toBeDefined();
+		expect(app.locals.settings.env).toEqual('test');
 	});
 });
