c4b7b53cdc68f1b9422a244e8c8688068d963cdb
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-06-30 04:01:55 +0100

    Added to check app status and registered routes

--- tests/integration/server.test.js
+++ tests/integration/server.test.js
@@ -1,6 +1,8 @@
 const {tearDownTestConnection} = require('../helpers/connection_teardown');
 const {connection} = require('mongoose');
 const app = require('../../server');
+const request = require('supertest')(app);
+const Joi = require('joi');
 
 describe('App', () => {
 	afterAll( async () => {
@@ -11,5 +13,30 @@ describe('App', () => {
 		expect(Object.keys((connection.models)).length).toBeGreaterThan(0);
 		expect(app).toBeDefined();
 		expect(app.locals.settings.env).toEqual('test');
+	});
+
+	it('should return app status with registered routes', (done) => {
+		request.get('/api/v1/status')
+			.set('Content-Type', 'application/json')
+			.expect('Content-Type', 'application/json; charset=utf-8')
+			.expect((response) => {
+
+				const appStatusSchema = Joi.object({
+					status: Joi.string().required(),
+					message: Joi.string().required(),
+					data: Joi.object({
+						info: Joi.object({
+							name: Joi.string().required(),
+							node_version: Joi.string().required(),
+							app_version: Joi.string().required()
+						}).options({allowUnknown: true}),
+						routes: Joi.array().min(1).required()
+					})
+				});
+
+				Joi.assert(response.body, appStatusSchema);
+			})
+			.expect(200)
+			.end(done);
 	});
 });
