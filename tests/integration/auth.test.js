a764dc1f84e27ccc4420e603cfd8d3e658d0999b
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-07-01 04:07:43 +0100

    Added lifecycle to update data value before each test case and tear down connection after all test cases

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -1,5 +1,21 @@
-describe('auth', () => {
-   it('should return success when registration is successful', () => {
+const {tearDownTestConnection} = require('../helpers/connection_teardown');
+const app = require('../../server');
 
+describe('Auth', () => {
+   let data = {};
+
+   beforeEach(() => {
+       data = {
+         firstname: 'Test',
+         lastname: 'User',
+         email:'test@example.com',
+         password: 'password',
+         passwordConfirmation: 'password'
+      }
    });
+
+   afterAll( async () => {
+       await tearDownTestConnection();
+   });
+
 });
