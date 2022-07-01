d81bfb8b3293515cf06c7a570fc9e8123f9f156b
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-07-01 04:10:19 +0100

    Test case to successfully register a user

--- tests/integration/auth.test.js
+++ tests/integration/auth.test.js
@@ -1,8 +1,11 @@
 const {tearDownTestConnection} = require('../helpers/connection_teardown');
+const {connection} = require('mongoose');
 const app = require('../../server');
+const Joi = require('joi');
+const request = require('supertest')(app);
 
 describe('Auth', () => {
-   let data = {};
+   let testUser, data = {};
 
    beforeEach(() => {
        data = {
@@ -18,4 +21,41 @@ describe('Auth', () => {
        await tearDownTestConnection();
    });
 
+   it('should successfully register a user', (done) => {
+      expect(Object.keys((connection.models)).length).toBeGreaterThan(0);
+
+      request.post('/api/v1/auth/signup')
+          .set('Content-Type', 'application/json')
+          .send(data)
+          .expect(200)
+          .expect('Content-Type', 'application/json; charset=utf-8')
+          .expect((response) => {
+             testUser = response.body;
+
+             const registeredUser = Joi.object({
+                status: Joi.string().required(),
+                message: Joi.string().required(),
+                data: Joi.object({
+                   token: Joi.string().required(),
+                   refreshToken: Joi.string().required(),
+                   user: Joi.object({
+                      firstname: data.firstname,
+                      lastname: data.lastname,
+                      email: data.email,
+                      role: Joi.string().required(),
+                      gender: Joi.string().required(),
+                      status: Joi.string().required(),
+                      avatar: Joi.string().required(),
+                      verificationToken: Joi.string().required(),
+                      isVerified: Joi.boolean().required(),
+                      createdAt: Joi.date().required(),
+                      updatedAt: Joi.date().required()
+                   }).options({allowUnknown: true})
+                })
+             });
+
+             Joi.assert(response.body, registeredUser);
+
+          }).end(done);
+   });
 });
