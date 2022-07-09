52f1cda2a56046809d85637ce462453525dd2668
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-07-09 20:27:38 +0100

    Refactored connection setup for easy connection to test or actual database

--- app/config/db/connect.js
+++ app/config/db/connect.js
@@ -4,19 +4,44 @@ const mongoose = require('mongoose');
 const {logger} = require('../../lib/utils');
 const {StatusCodes} = require('http-status-codes');
 const {CustomAPIError} = require('../../lib/errors');
+const {config} = require('../config');
+const {MongoMemoryServer} = require('mongodb-memory-server');
 
-const connectDB = async (url) => {
+const appEnv = config.app.env;
+const connectionStates = {
+    1: 'connected',
+    2: 'connecting'
+}
+
+module.exports = async (uri) => {
+    if (appEnv !== 'test') {
+        const message = {
+            success: 'Database connection established.',
+            error: 'Database connection error'
+        }
+        return connect(uri, message);
+    }
+
+    return MongoMemoryServer.create().then((mongoDBServer) => {
+        if (!Object.keys(connectionStates).includes(mongoose.connection.readyState.toString())) {
+            const msg = {
+                success: 'Test database connection established',
+                error: 'Test database connection error'
+            }
+            return connect(mongoDBServer.getUri(), msg);
+        }
+    });
+};
+
+function connect(uri, message) {
     try {
-        return await mongoose.connect(url)
-            .then(connection => {
-                logger.info('Database connection established.');
-                return connection;
+        return mongoose.connect(uri)
+            .then(() => {
+                logger.info(message.success);
             }).catch(err => {
-                throw new CustomAPIError(`Database connection error: ${err.message}`);
+                throw new CustomAPIError(`${message.error}: ${err.message}`);
             });
     } catch (err) {
         logger.error(`${StatusCodes.INTERNAL_SERVER_ERROR} - ${err.message}`);
     }
-};
-
-module.exports = connectDB;
+}
