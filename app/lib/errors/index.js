e7d08f2eb02066d7af15af9587baf10e4cda45e6
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-07-18 23:17:21 +0100

    Renamed file

--- app/lib/errors/index.js
+++ app/lib/errors/index.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const CustomAPIError = require('./custom-api');
+const CustomAPIError = require('./custom_api');
 const UnauthenticatedError = require('./unauthenticated');
-const NotFoundError = require('./not-found');
-const BadRequestError = require('./bad-request');
+const NotFoundError = require('./not_found');
+const BadRequestError = require('./bad_request');
 const UnauthorizedError = require('./unauthorized');
 
 module.exports = {
