dfaea276a2250d66fc9895623f0b8be8905824da
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-07-19 23:24:31 +0100

    Implemented function to handle encryption of text to hex

--- app/lib/utils/cryptography.js
+++ app/lib/utils/cryptography.js
@@ -1,11 +1,24 @@
 'use strict';
 
 const crypto = require('crypto');
+const securityKey = crypto.randomBytes(32);
+const algorithm = 'aes-256-cbc';
+const initVector = crypto.randomBytes(16);
 
-module.exports = (value) => {
+const createHash = (value) => {
 	if (value) {
 		return crypto.createHash('sha256').update(value).digest('hex');
 	}
 
 	return value;
 };
+
+const encrypt = (value) => {
+	const cipher = crypto.createCipheriv(algorithm, securityKey, initVector);
+	return  cipher.update(value, 'utf-8', 'hex') + cipher.final('hex');
+}
+
+module.exports = {
+	createHash,
+	encrypt,
+}
