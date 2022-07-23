17d39d555a3f307c27c871e56f0724ba9abd5a70
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-07-23 07:19:28 +0100

    Include social auth routes and handlers

--- app/routes/auth_routes.js
+++ app/routes/auth_routes.js
@@ -15,11 +15,11 @@ const {
 } = require('../controllers/authController');
 
 const {authenticateUser} = require('../middleware/authentication');
-const passportGoogle = require('../auth/google');
-const passportFacebook = require('../auth/facebook');
-const passportTwitter = require('../auth/twitter');
+const passportGoogle = require('../social_auth/google');
+const passportFacebook = require('../social_auth/facebook');
+const passportTwitter = require('../social_auth/twitter');
 
-router.route('/register').post(register);
+router.route('/signup').post(register);
 router.route('/login').post(login);
 router.route('/social-error').get(socialLoginError);
 router.route('/login/social').post(socialLogin);
@@ -32,19 +32,19 @@ router.route('/resend-verification-email').post(authenticateUser, resendVerifica
 router.route('/google/login').get(passportGoogle
 	.authenticate('google', {scope: [ 'email', 'profile' ]}, (_, __) => { /* auto redirected */} ));
 router.route('/google/callback').get(passportGoogle
-		.authenticate('google', { assignProperty: 'socialProfile', failureRedirect: '/api/v1/auth/social-error' }),
+		.authenticate('google', { assignProperty: 'socialProfile', failureRedirect: '/api/v1/social_auth/social-error' }),
 	(req, res) => socialLogin(req, res));
 
 router.route('/facebook/login').get(passportFacebook
 	.authenticate('facebook', { authType: 'reauthenticate'}, (_, __) => { /* auto redirected */}));
 router.route('/facebook/callback').get(passportFacebook
-		.authenticate('facebook', { assignProperty: 'socialProfile', failureRedirect: '/api/v1/auth/social-error' }),
+		.authenticate('facebook', { assignProperty: 'socialProfile', failureRedirect: '/api/v1/social_auth/social-error' }),
 	(req, res) => socialLogin(req, res));
 
 router.route('/twitter/login').get(passportFacebook
 	.authenticate('twitter', {}, (_, __) => { /* auto redirected */}));
 router.route('/twitter/callback').get(passportTwitter
-		.authenticate('twitter', { assignProperty: 'socialProfile', failureRedirect: '/api/v1/auth/social-error' }),
+		.authenticate('twitter', { assignProperty: 'socialProfile', failureRedirect: '/api/v1/social_auth/social-error' }),
 	(req, res) => socialLogin(req, res));
 
 module.exports = router;
