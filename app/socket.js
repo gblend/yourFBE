8114b8fc7761716aaf045217c6bead5c9e2aa264
Gabriel Ilochi <gabrielilo190@gmail.com>
2022-07-24 11:42:33 +0100

    Setup socket io client connection and disconnection events

--- app/socket.js
+++ app/socket.js
@@ -1,5 +1,7 @@
 const {Server} = require('socket.io');
 const {config} = require('./config/config');
+const {logger} = require('./lib/utils');
+const {socketErrorText} = require('./lib/utils/socketio_errors');
 const express = require('express');
 const app = express();
 const httpServer = require('http').createServer(app);
@@ -22,9 +24,26 @@ const io = new Server (httpServer, {
 	}
 });
 
+io.on('connection', (client) => {
+	logger.info(`New client: ${client.id} connected.`);
+	client.emit('connected', {status: true, message: 'You are now connected.'});
+
+	client.join('feeds');
+	client.join('categories');
+
+	client.on('disconnect', () => {
+		logger.info(`Client: ${client.id} disconnected.`);
+	});
+});
+
+io.on('connection_error', (error) => {
+	logger.error(`Connection error: ${socketErrorText(error.code)} - ${error.message}`);
+});
+
 
 module.exports = {
 	httpServer,
+	io,
 	express,
 	app,
 }
