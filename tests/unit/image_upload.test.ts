b7cab6d55c8a686e7cab16036b9fb9d006423c27
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-02-09 23:57:03 +0100

    add test case for local and cloudinary upload

--- tests/unit/image_upload.test.ts
+++ tests/unit/image_upload.test.ts
@@ -1,10 +1,10 @@
-import uploadImage from '../../app/lib/utils/image_upload';
-const cloudHelpers: any = {uploadImage};
+import {uploadImage, uploadToCloudinary, uploadToLocal} from '../../app/lib/utils/image_upload';
+const cloudHelpers: any = {uploadImage, uploadToCloudinary, uploadToLocal};
 
 describe('UploadImage', () => {
 	it('should return error without file to upload', async() => {
 		try {
-			await uploadImage({});
+			await uploadImage('');
 		} catch (err: any) {
 			expect(err.statusCode).toEqual(400);
 			expect(typeof(err.message)).toBe('string');
@@ -14,7 +14,7 @@ describe('UploadImage', () => {
 
 	it('should return error when file to upload is invalid', async () => {
 		try {
-			await uploadImage({files: {uploadImage: {mimetype: ''}}});
+			await uploadImage({uploadImage: {mimetype: ''}});
 		} catch (err: any) {
 			expect(err.statusCode).toEqual(400);
 			expect(typeof(err.message)).toBe('string');
@@ -24,7 +24,7 @@ describe('UploadImage', () => {
 
 	it('should return error when file to upload exceeds maximum size of 2mb (2097152)', async () => {
 		try {
-			await uploadImage({files: {uploadImage: {size: 3145728, mimetype: 'image/'}}});
+			await uploadImage({uploadImage: {size: 3145728, mimetype: 'image/'}});
 		} catch (err: any) {
 			expect(err.statusCode).toEqual(400);
 			expect(typeof(err.message)).toBe('string');
@@ -32,15 +32,40 @@ describe('UploadImage', () => {
 		}
 	});
 
-	it('should succeed when valid file is uploaded', async () => {
-		const req = {files: {uploadImage: {size: 1024, mimetype: 'image/'}}};
-		const _uploadImage = jest.spyOn(cloudHelpers, 'uploadImage');
-		const mockUploadImage = async (requestObj: any) => jest.fn().
-		mockReturnValue(await cloudHelpers.uploadImage(requestObj));
+	it('should succeed when valid file is uploaded - cloudinary', async () => {
+		const req = {files: {uploadImage: {size: 1024, mimetype: 'image/'}}, uploadType: 'cloudinary'};
+		const cloudinaryImageUploadSpy = jest.spyOn(cloudHelpers, 'uploadToCloudinary');
 
-		await mockUploadImage(req).catch(_ => _);
+		cloudinaryImageUploadSpy.mockImplementation((imageFile: any, uploadType) =>
+			Promise.resolve({url: 'http://localhost/upload/avatar.jpg'}));
+		await cloudHelpers.uploadToCloudinary(req.files.uploadImage, req.uploadType);
 
-		expect(_uploadImage).toHaveBeenCalledTimes(1);
-		expect(_uploadImage).toHaveBeenCalledWith(req);
+		expect(cloudinaryImageUploadSpy).toHaveBeenCalledTimes(1);
+		expect(cloudinaryImageUploadSpy).toHaveBeenCalledWith(req.files.uploadImage, req.uploadType);
 	});
+
+	it('should succeed when valid file is uploaded - local', async () => {
+		const req = {files: {uploadImage: {size: 1024, mimetype: 'image/'}}, uploadType: 'local'};
+		const imageUploadSpy = jest.spyOn(cloudHelpers, 'uploadImage');
+
+		imageUploadSpy.mockImplementation(async (file: any, type: any) =>
+			Promise.resolve({url: 'http://localhost/upload/avatar.jpg'}));
+		await cloudHelpers.uploadImage(req.files, req.uploadType);
+
+		expect(imageUploadSpy).toHaveBeenCalledTimes(1);
+		expect(imageUploadSpy).toHaveBeenCalledWith(req.files, req.uploadType);
+	});
+
+	it('should upload image to local storage ', async () => {
+		const req = {files: {uploadImage: {size: 1024, mimetype: 'image/'}}};
+		const localImageUploadSpy = jest.spyOn(cloudHelpers, 'uploadToLocal');
+
+		localImageUploadSpy.mockImplementation((imageFile: any) =>
+			Promise.resolve({url: 'http://localhost/upload/avatar.jpg'}));
+		await cloudHelpers.uploadToLocal(req.files.uploadImage);
+
+		expect(localImageUploadSpy).toHaveBeenCalledTimes(1);
+		expect(localImageUploadSpy).toHaveBeenCalledWith(req.files.uploadImage);
+	});
+
 });
