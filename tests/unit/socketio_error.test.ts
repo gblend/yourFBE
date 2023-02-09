a13f8877d17b5ec64f9228d30532ecb763cf2f1f
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-02-10 00:01:14 +0100

    add test cases for socket error code to text mapping

---
+++ tests/unit/socketio_error.test.ts
@@ -0,0 +1,15 @@
+import {socketErrorText} from '../../app/lib/utils/socketio_errors';
+
+describe('SocketErrorText', () => {
+    it('should return text for passed error code', function () {
+        const text: string = socketErrorText(1);
+
+        expect(text).toBe('Session ID unknown');
+    });
+
+    it('should return empty string when out of range code is passed', function () {
+        const text: string = socketErrorText(6);
+
+        expect(text).toBe('');
+    });
+});
