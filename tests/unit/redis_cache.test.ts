7614c64b0589d4aa968fd315e26475806d2bad6e
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-02-13 01:39:39 +0100

    add test cases for redis get, set and del

--- tests/unit/redis_cache.test.ts
+++ tests/unit/redis_cache.test.ts
@@ -2,18 +2,52 @@ import {
     redisDelete,
     redisSet,
     redisFlushAll,
-    redisRefreshCache,
     redisGet,
-    redisGetBatchRecords,
-    redisSetBatchRecords,
     initRedisCache
 } from '../../app/lib/utils';
 
 describe('Redis', () => {
-    it('should return redis connection', function () {
-        const redis = initRedisCache();
 
-        expect(redis).toBeDefined;
+    const cache = {
+        redisDelete,
+        redisSet,
+        redisFlushAll,
+        redisGet,
+        initRedisCache
+    }
+
+    it('should return redis connection', () => {
+        const initCacheSpy = jest.spyOn(cache, 'initRedisCache');
+        initCacheSpy.mockImplementation(() => Promise.resolve(() => {}));
+        const mockIoredis = cache.initRedisCache();
+
+        expect(mockIoredis).toBeDefined();
+        expect(initCacheSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should return null for non existing cache key', async () => {
+        const redisGetSpy = jest.spyOn(cache, 'redisGet');
+        redisGetSpy.mockImplementation((key: string) => Promise.resolve(null));
+        const cacheValue = await cache.redisGet('test');
+
+        expect(cacheValue).toBe(null);
+        expect(redisGetSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should set value for a provided key', async () => {
+        const redisSetSpy = jest.spyOn(cache, 'redisSet');
+        redisSetSpy.mockImplementation((key: string) => Promise.resolve('testValue'));
+        await cache.redisSet('test', 'testValue');
+
+        expect(redisSetSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('should delete the value for a provided key', async () => {
+        const redisDeleteSpy = jest.spyOn(cache, 'redisDelete');
+        redisDeleteSpy.mockImplementation((key: string) => Promise.resolve(null));
+        await cache.redisDelete('test');
+
+        expect(redisDeleteSpy).toHaveBeenCalledTimes(1);
     });
 })
 
