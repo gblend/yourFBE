8828de6d6a47352a9968e26c6c052c06988193d5
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-03-03 01:04:31 +0100

    add api documentation route

---
+++ app/routes/api_doc.ts
@@ -0,0 +1,13 @@
+import {Router} from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../../docs';
+
+const router = Router();
+const options = {
+    explorer: true
+};
+
+router.use('/api/docs', swaggerUi.serve);
+router.route('/api/docs').get(swaggerUi.setup(swaggerDocument, options));
+
+export default router;
