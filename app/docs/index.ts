588d742484455ffb682744154925af537afc5892
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-06 03:15:47 +0100

    Added bearer and cookie security schemes

--- app/docs/index.ts
+++ app/docs/index.ts
@@ -5,6 +5,18 @@ import generalSetup from './general';
 export default {
     ...generalSetup,
     components: {
+        securitySchemes: {
+            cookieAuth: {
+                type: 'apiKey',
+                in: 'cookie',
+                name: 'accessToken',
+            },
+            bearerAuth: {
+                type: 'http',
+                scheme: 'bearer',
+                bearerFormat: 'JWT'
+            }
+        },
         ...schemas,
         headers: {
             contentType: 'application/json',
