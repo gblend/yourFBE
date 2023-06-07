856b0049424d4761f9327b75656c75efcc619c75
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-07 07:33:34 +0100

    Added doc reusable partials

---
+++ app/docs/paths/partials.ts
@@ -0,0 +1,32 @@
+export default {
+    authUser: {
+        firstname: 'John',
+        lastname: 'Doe',
+        email: 'johndoe@example.com',
+        role: 'user',
+        gender: 'male',
+        status: 'enabled',
+        socialChannel: 'facebook',
+        socialChannelId: 'xx**xx',
+        avatar: '/uploads/default_avatar.jpeg',
+        verificationToken: 'arh399027ann3m4',
+        isVerified: true,
+        verified: '1970-01-01 01:01:00',
+        lastLogin: '1970-01-01 01:01:00',
+        createdAt: '2022-08-21T21:16:24.399Z',
+        updatedAt: '2023-03-26T23:24:14.515Z',
+    },
+    cookieAuth: {
+        cookieAccessToken: [],
+        cookieRefreshToken: []
+    },
+    pagination: {
+        pageSize: 10,
+        pageNumber: 1,
+        offset: 0,
+        total: 10,
+        pages: 1,
+        previous: 1,
+        next: 1
+    }
+}
