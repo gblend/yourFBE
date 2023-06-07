4586295de0f82cc1dedf3af23d4c588e2924c0ee
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-07 07:32:58 +0100

    Added entry point for controllers

---
+++ app/controllers/index.ts
@@ -0,0 +1,110 @@
+export {
+    register,
+    login,
+    logout,
+    verifyEmail,
+    resetPassword,
+    forgotPassword,
+    resendVerificationEmail,
+    socialLogin,
+    socialLoginError
+} from './authController';
+
+export {
+    getAllConfig,
+    createConfig,
+    getSingleConfig,
+    updateConfig,
+    disableConfig,
+    deleteConfig,
+    getConfigByPath
+} from './configController';
+
+export {
+    getFeedsByCategory,
+    createFeed,
+    deleteFeed,
+    disableFeedById,
+    toggleFeedsStatusByCategoryId,
+    getFeedById,
+    getFeeds,
+    getFeedsByCategoryId,
+    updateFeed,
+    getPostsByFeedId,
+    randomFeedsPosts,
+    recentFeedPosts
+} from './feedController';
+
+export {
+    getCategories,
+    createCategory,
+    updateCategory,
+    deleteCategory,
+    disableCategory,
+    getCategoryById
+} from './feedCategoryController';
+
+export {
+    followFeed,
+    unfollowFeed,
+    unfollowAllFeeds,
+    latestPostsByFollowedFeeds,
+    getFollowedFeeds,
+    feedsFollowersStats,
+    followAllFeedsInCategory,
+    unfollowAllFeedsInCategory
+} from './followedFeedController';
+
+export {
+    getActivityLogs,
+    getActivityLog,
+    getPollingLogs,
+    getPollingLog,
+    searchLogs
+} from './logController';
+
+export {
+    createNotification,
+    deleteNotification,
+    updateNotification,
+    getNotifications
+} from './notificationController';
+
+export {
+    savePostForLater,
+    deletePostSavedForLater,
+    getPostsSavedForLater,
+    getPostSavedForLater,
+    markPostSavedForLaterAsRead,
+    userStarredFeedPostsStats,
+    allStarredFeedPostsStats
+} from './savedForLaterController';
+
+export {
+    search
+} from './searchController';
+
+export {
+    adminDashboardStats,
+    userDashboardStats
+} from './statisticController.js';
+
+export {
+    getAllUsers,
+    getAllAdmins,
+    getUser,
+    updateUser,
+    updatePassword,
+    showCurrentUser,
+    enableUserAccount,
+    getDisabledAccounts,
+    disableUserAccount
+} from './userController';
+
+export {
+    uploadProfileImage
+} from './uploadController';
+
+export {
+    logScheduledProcess
+} from './scheduleController';
