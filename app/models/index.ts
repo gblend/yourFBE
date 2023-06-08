a905ecb738846e2fd19fa27c27dff64cd02c8a1e
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-08 07:38:30 +0100

    Added entry point for models

---
+++ app/models/index.ts
@@ -0,0 +1,61 @@
+export {
+    ActivityLog,
+    validateActivityLogDto
+} from './ActivityLog';
+
+export {
+    ConfigData,
+    validateConfigDataDto
+} from './Config';
+
+export {
+    Feed,
+    validateFeedDto,
+    validateFeedUpdateDto,
+} from './Feed';
+
+export {
+    FeedCategory,
+    validateFeedCategoryDto
+} from './FeedCategory';
+
+export {
+    FollowedFeed,
+    validateFollowFeedsInCategoryDto,
+    validateFollowedFeedDto
+} from './FollowedFeed';
+
+export {
+    Notification,
+    validateNotificationDto,
+    validateUpdateNotificationDto
+} from './Notification';
+
+export {
+    NotificationLog,
+    validateNotificationLogDto,
+} from './NotificationLog';
+
+export {
+    PollingLog,
+    validatePollingLogDto
+} from './PollingLog';
+
+export {
+    SavedForLater,
+    validateSavedForLaterDto
+} from './SavedForLater';
+
+export {Token} from './Token';
+
+export {
+    User,
+    validateUserDto,
+    validateLogin,
+    validateUpdateUser,
+    validateUpdatePassword
+} from './User'
+
+export {
+    Schedule
+} from './Schedule'
