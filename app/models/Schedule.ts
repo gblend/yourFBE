7f39f597facdddafc38c12466cf2bce5fe0935b1
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-08 07:37:48 +0100

    Created schedule model

---
+++ app/models/Schedule.ts
@@ -0,0 +1,28 @@
+import mongoose, {model, Schema} from 'mongoose';
+import {ISchedule, ScheduleModel} from '../interface';
+
+const ScheduleSchema: mongoose.Schema<ISchedule, ScheduleModel> = new Schema<ISchedule, ScheduleModel>({
+    code: {
+        type: String,
+        required: true,
+    },
+    started_at: {
+        type: Date,
+        required: true
+    },
+    ended_at: {
+        type: Date,
+        default: null
+    },
+    status: {
+        type: String,
+        enum: {
+            values: ['pending', 'running', 'completed'],
+            messages: '{VALUE} is not acceptable.'
+        },
+        default: 'pending',
+    }
+}, {timestamps: true});
+
+export const Schedule: ScheduleModel = model<ISchedule, ScheduleModel>('Schedule', ScheduleSchema);
+
