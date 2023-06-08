6ef84da46d55f7d442e34b83eb75c280ed345080
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-08 07:37:03 +0100

    Added entry point for routes

---
+++ app/routes/index.ts
@@ -0,0 +1,29 @@
+import apiDocRouter from './api_doc';
+import feedCategoryRouter from './feed_category';
+import followedFeedRouter from './followed_feed';
+import savedForLaterRouter from './saved_for_later';
+import authRouter from './auth';
+import userRouter from './user';
+import searchRouter from './search';
+import feedRouter from './feed';
+import statRouter from './stat';
+import logRouter from './log';
+import postRouter from './post';
+import configRouter from './config';
+import notificationRouter from './notification';
+
+export {
+    apiDocRouter,
+    feedCategoryRouter,
+    followedFeedRouter,
+    savedForLaterRouter,
+    authRouter,
+    userRouter,
+    searchRouter,
+    feedRouter,
+    statRouter,
+    logRouter,
+    postRouter,
+    configRouter,
+    notificationRouter,
+}
