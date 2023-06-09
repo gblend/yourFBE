8ae66d7108f118e68e062a3c12ed7f8ca319a62c
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-09 13:40:45 +0100

    Created schedule interface

---
+++ app/interface/schedule.ts
@@ -0,0 +1,11 @@
+import {Model} from 'mongoose';
+
+export interface ISchedule {
+    code: string,
+    started_at: Date,
+    ended_at?: Date,
+    status?: string,
+}
+
+export interface ScheduleModel extends Model<ISchedule> {
+}
