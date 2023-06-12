f960b13990c64c825eac16407fcd0480331dfa04
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-12 15:32:14 +0100

    Refactor

--- app/middleware/event_handler.ts
+++ app/middleware/event_handler.ts
@@ -5,16 +5,12 @@ export const eventHandler = (event: string) => {
 	return (err: any) => {
 		switch (event) {
 			case 'SIGTERM':
-				logger.error(`${event} signal received - closing http server.`);
-				if (mongoose.connection.readyState === 1) {
-					mongoose.connection.close(false, () => {
-						process.exit(err ? 1 : 0);
-					});
-				}
-				break;
 			case 'uncaughtException':
-				logger.error(`${event} - ${err.stack}`);
-				process.exit(1);
+				logger.error(`${event} - closing http server: ${err?.message}`);
+				if (mongoose.connection.readyState === 1) {
+					mongoose.connection.close(false, () => {} );
+				}
+				process.exit(err ? 1 : 0);
 				break;
 			case 'unhandledRejection':
 			default:
