889d9d238eb9db8cb4baa352d19b088c6b0d0a99
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-20 08:38:25 +0100

    Replace hard coded status codes

--- app/middleware/error_handler.ts
+++ app/middleware/error_handler.ts
@@ -11,28 +11,28 @@ export const errorHandler = (err: any, req: Request, res: Response, _next: NextF
         message: err.message || 'Something went wrong. Please try again later.',
     };
 
-    if (err.message.indexOf('already in use')) {
+    if (err.message.indexOf('already in use') != -1) {
         customError.message = err.message;
-        customError.statusCode = 400;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
     }
 
     if (err.name === 'ValidationError') {
         customError.message = Object.values(err.errors)
             .map((item: any) => item.message)
             .join(',');
-        customError.statusCode = 400;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
     }
 
     if (err.code && err.code === 11000) {
         customError.message = `Duplicate value entered for ${Object.keys(
             err.keyValue
         )} field, please choose another value.`;
-        customError.statusCode = 400;
+        customError.statusCode = StatusCodes.BAD_REQUEST;
     }
 
     if (err.name === 'CastError') {
         customError.message = `No resource found with id: ${err.value}`;
-        customError.statusCode = 404;
+        customError.statusCode = StatusCodes.NOT_FOUND;
     }
 
     logger.error(`${customError.statusCode} - ${customError.message} - ${method} ${path}`);
