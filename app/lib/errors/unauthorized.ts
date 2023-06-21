1ded4af7930536e5b43485f2754b3523a6765ed8
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-21 06:48:51 +0100

    Pass status code to super constructor

--- app/lib/errors/unauthorized.ts
+++ app/lib/errors/unauthorized.ts
@@ -2,11 +2,9 @@ import {StatusCodes} from 'http-status-codes';
 import CustomAPIError from './custom_api';
 
 class UnauthorizedError extends CustomAPIError {
-    readonly statusCode: number;
 
     constructor(message: string) {
-        super(message);
-        this.statusCode = StatusCodes.FORBIDDEN;
+        super(message, StatusCodes.FORBIDDEN);
     }
 }
 
