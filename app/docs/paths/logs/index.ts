cf71fb695248d846934cfb0348030aaca52e10ab
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-06-24 06:58:54 +0100

    Added documentation for polling logs

--- app/docs/paths/logs/index.ts
+++ app/docs/paths/logs/index.ts
@@ -111,5 +111,127 @@ export default {
 
             }
         }
+    },
+    '/logs/polling': {
+        get: {
+            tags: ['Logs'],
+            description: 'Get polling logs',
+            summary: 'retrieve polling logs',
+            operationId: 'pollingLogs',
+            security: [{
+                bearerAuth: []
+            }],
+            parameters: [
+                {
+                    name: 'pageNumber',
+                    in: 'query',
+                    description: 'Page number of activity log list',
+                    type: 'number',
+                    example: 1
+                },
+                {
+                    name: 'pageSize',
+                    in: 'query',
+                    description: 'The number of records to be retrieved',
+                    type: 'number',
+                    example: 10
+                }
+            ],
+            responses: {
+                '200': {
+                    description: 'Polling logs successful response',
+                    content: {
+                        'application/json': {
+                            example: {
+                                status: 'success',
+                                message: 'Polling logs fetched successfully',
+                                data: {
+                                    logs: [
+                                        {
+                                            _id: '62b35c71ba7af91ee98e97ee',
+                                            action: 'disableConfig: 62b34a8eb5d378221169d1cd - PATCH /disable/62b34a8eb5d378221169d1cd - by admin',
+                                            resourceName: 'ConfigData',
+                                            user: '629fc2e6e4c459df61f2b1ad',
+                                            createdAt: '2022-06-22T18:16:17.177Z',
+                                            updatedAt: '2022-06-22T18:16:17.177Z',
+                                            __v: 0
+                                        },
+                                        {
+                                            _id: '62b360ec3533a418e92b8bb0',
+                                            action: 'disableConfig: 62b34a8eb5d378221169d1cd - PATCH /disable/62b34a8eb5d378221169d1cd - by admin',
+                                            resourceName: 'ConfigData',
+                                            user: '629fc2e6e4c459df61f2b1ad',
+                                            createdAt: '2022-06-22T18:35:24.985Z',
+                                            updatedAt: '2022-06-22T18:35:24.985Z',
+                                            __v: 0
+                                        },
+                                    ],
+                                    pagination: partials.pagination
+                                }
+                            }
+                        }
+                    }
+                },
+
+            }
+        }
+    },
+    '/logs/polling/{id}': {
+        get: {
+            tags: ['Logs'],
+            description: 'Get polling log',
+            summary: 'retrieve polling log',
+            operationId: 'pollingLog',
+            security: [{
+                bearerAuth: []
+            }],
+            parameters: [
+                {
+                    name: 'id',
+                    in: 'path',
+                    description: 'The id of the polling log to retrieve',
+                    type: 'string',
+                    example: '62b35c71ba7af91ee98e97ee'
+                },
+            ],
+            responses: {
+                '200': {
+                    description: 'Polling log successful response',
+                    content: {
+                        'application/json': {
+                            example: {
+                                status: 'success',
+                                message: 'Polling log fetched successfully',
+                                data: {
+                                    logs: [
+                                        {
+                                            _id: '62b35c71ba7af91ee98e97ee',
+                                            action: 'disableConfig: 62b34a8eb5d378221169d1cd - PATCH /disable/62b34a8eb5d378221169d1cd - by admin',
+                                            resourceName: 'ConfigData',
+                                            user: '629fc2e6e4c459df61f2b1ad',
+                                            createdAt: '2022-06-22T18:16:17.177Z',
+                                            updatedAt: '2022-06-22T18:16:17.177Z',
+                                            __v: 0
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    }
+                },
+                '404': {
+                    description: 'Polling log not found',
+                    content: {
+                        'application/json': {
+                            example: {
+                                status: 'error',
+                                message: 'Polling log does not exist.',
+                                data: {}
+                            }
+                        }
+                    }
+                },
+            }
+        }
     }
 }
