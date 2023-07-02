8ce477ded82af45b7bf78d485121c54e961d9168
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-07-02 09:27:16 +0100

    Refactor

--- app/lib/utils/xss_clean.ts
+++ app/lib/utils/xss_clean.ts
@@ -1,37 +1,36 @@
-import  xssFilters  from 'xss-filters'
-import {NextFunction, Response, Request} from '../../types'
+import xssFilters from 'xss-filters';
+import { NextFunction, Response, Request } from '../../types';
 
 /**
  * Clean for xss.
  * @param {string/object} data - The value to sanitize
  * @return {string/object} The sanitized value
  */
-const clean  = (data: unknown = ''): any => {
-    let isObject = false
-    if (typeof data === 'object') {
-        data = JSON.stringify(data)
-        isObject = true
-    }
+const clean = (data: unknown = ''): any => {
+  let isObject = false;
+  if (typeof data === 'object') {
+    data = JSON.stringify(data);
+    isObject = true;
+  }
 
-    data = xssFilters.inHTMLData(data).trim()
-    if (isObject && typeof data === 'string') {
-        data = JSON.parse(data)
-    }
+  data = xssFilters.inHTMLData(data).trim();
+  if (isObject && typeof data === 'string') {
+    data = JSON.parse(data);
+  }
 
-    return data
-}
-
+  return data;
+};
 
 /**
  * Middleware to clean request body, query and params for xss
  * @return {function} Middleware function
  */
 export const xss = () => {
-    return (req: Request, res: Response, next: NextFunction) => {
-        if (req.body) req.body = clean(req.body)
-        if (req.query) req.query = clean(req.query)
-        if (req.params) req.params = clean(req.params)
+  return (req: Request, res: Response, next: NextFunction) => {
+    if (req.body) req.body = clean(req.body);
+    if (req.query) req.query = clean(req.query);
+    if (req.params) req.params = clean(req.params);
 
-        next()
-    }
-}
+    next();
+  };
+};
