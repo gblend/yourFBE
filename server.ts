6d1c6b4fb0082b895b6253321c73ddec53e55884
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-07-03 15:00:24 +0100

    Export app server for test purpose

--- server.ts
+++ server.ts
@@ -1,20 +1,20 @@
 import 'express-async-errors';
-import {app, appEnv, config, connectDB, httpServer, initCron, logger,} from './app';
-import {constants} from './app/lib/utils';
+import { appEnv, config, connectDB, httpServer, initCron, logger } from './app';
+import { constants } from './app/lib/utils';
 
-const {port, baseUrl, name} = config.app;
+const { port, baseUrl, name } = config.app;
 
-const start =  async (): Promise<void> => {
-	connectDB().then(() => {
-		if (constants.envList.includes(appEnv)) {
-			httpServer.listen(port, () => {
-				logger.info(`${name} server running: ${baseUrl}\n ${baseUrl}/api-docs`);
-			});
-			initCron();
-		}
-	});
-}
+const start = async (): Promise<void> => {
+  connectDB().then(() => {
+    if (constants.envList.includes(appEnv)) {
+      httpServer.listen(port, () => {
+        logger.info(`${name} server running: ${baseUrl}\n ${baseUrl}/api-docs`);
+      });
+      initCron();
+    }
+  });
+};
 
-start().then((v: void) => v)
+start().then((v: void) => v);
 
-export default app;
+export default httpServer;
