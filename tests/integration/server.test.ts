735f38da50fcdca440c150044fb667322165368b
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-07-05 18:22:10 +0100

    Refactor

--- tests/integration/server.test.ts
+++ tests/integration/server.test.ts
@@ -1,43 +1,43 @@
-import {tearDownTestConnection} from '../helpers/connection_teardown';
-import {connection} from 'mongoose';
-import app from '../../server';
+import { tearDownTestConnection } from '../helpers/connection_teardown';
+import { connection } from 'mongoose';
+import { app } from '../../app';
 import supertest from 'supertest';
 const request = supertest(app);
 import Joi from 'joi';
 
 describe('App', () => {
-	afterAll( async () => {
-		await tearDownTestConnection();
-	});
+  afterAll(async () => {
+    await tearDownTestConnection();
+  });
 
-	it('should successfully initialize app instance',() => {
-		expect(Object.keys((connection.models)).length).toBeGreaterThan(0);
-		expect(app).toBeDefined();
-		expect(app.locals.settings.env).toEqual('test');
-	});
+  it('should successfully initialize app instance', () => {
+    expect(Object.keys(connection.models).length).toBeGreaterThan(0);
+    expect(app).toBeDefined();
+    expect(app.locals.settings.env).toEqual('test');
+  });
 
-	it('should return app status with registered routes', (done) => {
-		request.get('/api/v1/status')
-			.set('Content-Type', 'application/json')
-			.expect('Content-Type', 'application/json; charset=utf-8')
-			.expect((response: any) => {
+  it('should return app status with registered routes', (done) => {
+    request
+      .get('/api/v1/status')
+      .set('Content-Type', 'application/json')
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect((response: any) => {
+        const appStatusSchema = Joi.object({
+          status: Joi.string().required(),
+          message: Joi.string().required(),
+          data: Joi.object({
+            info: Joi.object({
+              name: Joi.string().required(),
+              node_version: Joi.string().required(),
+              app_version: Joi.string().required(),
+            }).options({ allowUnknown: true }),
+            routes: Joi.array(),
+          }),
+        });
 
-				const appStatusSchema = Joi.object({
-					status: Joi.string().required(),
-					message: Joi.string().required(),
-					data: Joi.object({
-						info: Joi.object({
-							name: Joi.string().required(),
-							node_version: Joi.string().required(),
-							app_version: Joi.string().required()
-						}).options({allowUnknown: true}),
-						routes: Joi.array()
-					})
-				});
-
-				Joi.assert(response.body, appStatusSchema);
-			})
-			.expect(200)
-			.end(done);
-	});
+        Joi.assert(response.body, appStatusSchema);
+      })
+      .expect(200)
+      .end(done);
+  });
 });
