841850b0c69659badd017ceee709f14f59225aa4
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-07-06 09:24:42 +0100

    Prettier format

--- app/config/db/connect.ts
+++ app/config/db/connect.ts
@@ -1,39 +1,46 @@
 import mongoose from 'mongoose';
-import {constants, logger} from '../../lib/utils';
-import {CustomAPIError} from '../../lib/errors';
-import {config} from '../config';
-import {MongoMemoryServer} from 'mongodb-memory-server';
+import { constants, logger } from '../../lib/utils';
+import { CustomAPIError } from '../../lib/errors';
+import { config } from '../config';
+import { MongoMemoryServer } from 'mongodb-memory-server';
 
 const appEnv = config.app.env;
 const connectionStates = {
-    1: 'connected',
-    2: 'connecting'
-}
+  1: 'connected',
+  2: 'connecting',
+};
 
 export const connectDB = async (): Promise<void> => {
-    const uri = config.database.uri;
-    const message = {
-        success: 'Database connection established.',
-        error: 'Database connection error'
-    }
+  const uri = config.database.uri;
+  const message = {
+    success: 'Database connection established.',
+    error: 'Database connection error',
+  };
 
-    if (constants.envList.includes(appEnv)) {
-        return await connect(uri, message);
-    }
+  if (constants.envList.includes(appEnv)) {
+    return await connect(uri, message);
+  }
 
-    return MongoMemoryServer.create().then((mongoDBServer: MongoMemoryServer) => {
-        if (!Object.keys(connectionStates).includes(mongoose.connection.readyState.toString())) {
-            return connect(mongoDBServer.getUri(), message);
-        }
-    });
-}
-
-const connect = async (uri: string, message: { success: string, error: string }): Promise<void> => {
-    try {
-        mongoose.set('strictQuery', true);
-        await mongoose.connect(uri);
-        logger.info(message.success);
-    } catch (err: any) {
-        throw new CustomAPIError(`${message.error}: ${err.message}`);
+  return MongoMemoryServer.create().then((mongoDBServer: MongoMemoryServer) => {
+    if (
+      !Object.keys(connectionStates).includes(
+        mongoose.connection.readyState.toString(),
+      )
+    ) {
+      return connect(mongoDBServer.getUri(), message);
     }
-}
+  });
+};
+
+const connect = async (
+  uri: string,
+  message: { success: string; error: string },
+): Promise<void> => {
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(uri);
+    logger.info(message.success);
+  } catch (err: any) {
+    throw new CustomAPIError(`${message.error}: ${err.message}`);
+  }
+};
