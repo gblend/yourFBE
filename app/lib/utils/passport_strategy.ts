5d3becee30b49e2980808d691a08c01623e12491
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-07-23 20:16:03 +0100

    Setup passport strategy factory

---
+++ app/lib/utils/passport_strategy.ts
@@ -0,0 +1,79 @@
+import MockStrategy from 'passport-mock-strategy';
+import {
+  Profile as FacebookProfile,
+  Strategy as FacebookStrategy,
+} from 'passport-facebook';
+import {
+  Profile as GoogleProfile,
+  Strategy as GoogleStrategy,
+} from 'passport-google-oauth20';
+import {
+  Profile as TwitterProfile,
+  Strategy as TwitterStrategy,
+} from 'passport-twitter';
+import { config } from '../../config/config';
+import { registerSocialProfile } from '../../socialauth';
+import { constants } from './constant';
+import { appEnv } from '../../../app';
+import { CustomAPIError } from '../errors';
+
+export const getStrategy = (name: string = ''): any => {
+  if (constants.envList.includes(appEnv)) {
+    return new MockStrategy();
+  }
+
+  let options: any = {};
+  let _Strategy: any = null;
+  const verify = async (
+    _accessToken: string,
+    _refreshToken: string,
+    profile: FacebookProfile | GoogleProfile | TwitterProfile,
+    cb: (...args: any) => any,
+  ): Promise<any> => {
+    return registerSocialProfile(profile, cb, name);
+  };
+
+  switch (name) {
+    case 'facebook':
+      _Strategy = FacebookStrategy;
+      options = {
+        clientID: config.auth[name].clientID,
+        clientSecret: config.auth[name].clientSecret,
+        callbackURL: config.auth[name].callbackURL,
+        profileFields: [
+          'id',
+          'displayName',
+          'photos',
+          'email',
+          'gender',
+          'name',
+          'profileUrl',
+        ],
+        enableProof: true,
+        authType: 'reauthenticate',
+      };
+      break;
+    case 'google':
+      _Strategy = GoogleStrategy;
+      options = {
+        clientID: config.auth[name].clientID,
+        clientSecret: config.auth[name].clientSecret,
+        callbackURL: config.auth[name].callbackURL,
+      };
+      break;
+    case 'twitter':
+      _Strategy = TwitterStrategy;
+      options = {
+        consumerKey: config.auth[name].consumerKey,
+        consumerSecret: config.auth[name].consumerSecret,
+        callbackURL: config.auth[name].callbackURL,
+      };
+      break;
+    default:
+      throw new CustomAPIError('Strategy not implemented');
+  }
+
+  const strategy = new _Strategy(options, verify);
+
+  return strategy;
+};
