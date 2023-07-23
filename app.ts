2af9dd6cefc390718d5b0a1e4601b1e0faaa0e40
Gabriel Ilochi <gabrielilo190@gmail.com>
2023-07-23 05:15:40 +0100

    Setup newrelic agent express instrumentation

--- app.ts
+++ app.ts
@@ -9,53 +9,78 @@ import cookieParser from 'cookie-parser';
 import fileUpload from 'express-fileupload';
 import rateLimit from 'express-rate-limit';
 import mongoSanitize from 'express-mongo-sanitize';
-import {config} from './app/config/config';
-import {app, express, httpServer} from './app/socket';
-import {constants, decodeCookies, getRedisConnection, logger, serverStatus, xss} from './app/lib/utils';
-import sentryErrorHandler, {sentryRequestHandler, sentryTracingHandler} from './sentry';
-import {errorHandler, eventHandler, responseInterceptor, routeNotFound} from './app/middleware';
+import { config } from './app/config/config';
+import { app, express, httpServer } from './app/socket';
 import {
-	apiDocRouter,
-	authRouter,
-	configRouter,
-	feedCategoryRouter,
-	feedRouter,
-	followedFeedRouter,
-	logRouter,
-	notificationRouter,
-	postRouter,
-	savedForLaterRouter,
-	searchRouter,
-	statRouter,
-	userRouter,
+  constants,
+  decodeCookies,
+  getRedisConnection,
+  logger,
+  serverStatus,
+  xss,
+} from './app/lib/utils';
+import sentryErrorHandler, {
+  sentryRequestHandler,
+  sentryTracingHandler,
+} from './sentry';
+import {
+  errorHandler,
+  eventHandler,
+  responseInterceptor,
+  routeNotFound,
+} from './app/middleware';
+import {
+  apiDocRouter,
+  authRouter,
+  configRouter,
+  feedCategoryRouter,
+  feedRouter,
+  followedFeedRouter,
+  logRouter,
+  notificationRouter,
+  postRouter,
+  savedForLaterRouter,
+  searchRouter,
+  statRouter,
+  userRouter,
 } from './app/routes';
 
-export {initCron} from './app/scheduler';
-export {connectDB} from './app/config/db/connect';
-const {env: appEnv, prefix} = config.app;
+export { initCron } from './app/scheduler';
+export { connectDB } from './app/config/db/connect';
+const { env: appEnv, prefix } = config.app;
 
 cloudinary.v2.config({
-	cloud_name: config.cloudinary.cloudName,
-	api_key: config.cloudinary.cloudApiKey,
-	api_secret: config.cloudinary.cloudApiSecret
+  cloud_name: config.cloudinary.cloudName,
+  api_key: config.cloudinary.cloudApiKey,
+  api_secret: config.cloudinary.cloudApiSecret,
 });
 
 const apiRateLimiter = rateLimit({
-	windowMs: config.rateLimiter.windowMs,
-	max: config.rateLimiter.max,
-	standardHeaders: true,
+  windowMs: config.rateLimiter.windowMs,
+  max: config.rateLimiter.max,
+  standardHeaders: true,
 });
 
-let sessionRedisStore: any = {}
+let sessionRedisStore: any = {};
 if (constants.envList.includes(appEnv)) {
-	import('connect-redis').then((RedisStore) => {
-		sessionRedisStore = {
-			store: new RedisStore.default({
-				prefix: config.app.name,
-				client: getRedisConnection()
-			})
-		}
-	});
+  import('newrelic').then((newrelicModule) => {
+    const newrelic = newrelicModule.default;
+    // instrument express after the agent has been loaded
+    newrelic.instrumentLoadedModule('express', express);
+  });
+
+  import('connect-redis')
+    .then((redisStoreModule) => {
+      const RedisStore = redisStoreModule.default;
+      const redisClient = getRedisConnection();
+      sessionRedisStore = {
+        store: new RedisStore({
+          prefix: config.app.name,
+          client: redisClient,
+        }),
+      };
+    })
+    .catch((err: Error) => err);
 }
 
 app.use(helmet());
@@ -63,21 +88,23 @@ app.use(xss());
 app.use(mongoSanitize());
 app.set('trust proxy', 1);
 app.use('/api', apiRateLimiter);
-app.use(express.json({limit: '300kb'}));
-app.use(express.urlencoded({extended: false}));
+app.use(express.json({ limit: '300kb' }));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(config.jwt.secret));
 app.use(decodeCookies);
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(fileUpload({useTempFiles: true}));
+app.use(fileUpload({ useTempFiles: true }));
 if (appEnv === 'development') app.use(morgan('dev'));
 
-app.use(session({
-	...sessionRedisStore,
-	secret:config.session.secret,
-	resave: false,
-	saveUninitialized: true
-}));
+app.use(
+  session({
+    ...sessionRedisStore,
+    secret: config.session.secret,
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -103,14 +130,8 @@ app.use(errorHandler);
 app.use(sentryErrorHandler);
 
 process
-	.on('SIGTERM', eventHandler('SIGTERM'))
-	.on('unhandledRejection', eventHandler('unhandledRejection'))
-	.on('uncaughtException', eventHandler('uncaughtException'));
+  .on('SIGTERM', eventHandler('SIGTERM'))
+  .on('unhandledRejection', eventHandler('unhandledRejection'))
+  .on('uncaughtException', eventHandler('uncaughtException'));
 
-export {
-	logger,
-	config,
-	httpServer,
-	appEnv,
-	app,
-}
+export { logger, config, httpServer, appEnv, app };
